d6c6b4ba83073134beb6fab2e2b4e8827f4e48d2
richarddushime <mudaherarich@gmail.com>
2021-09-20 17:04:12 +0300

    esperance

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  console.log("App Executed");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ets Shop Esperance Welcomes you </Text>
       <StatusBar style="auto" />
     </View>
   );
